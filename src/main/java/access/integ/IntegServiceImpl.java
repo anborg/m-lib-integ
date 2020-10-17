3b2e83e454adc9900368fddc00419f0b3744de50
anborg <dev@gmail.com>
2020-10-17 00:11:37 -0400

    ss ifc change

--- src/main/java/access/integ/IntegServiceImpl.java
+++ src/main/java/access/integ/IntegServiceImpl.java
@@ -3,9 +3,7 @@ package access.integ;
 import muni.dao.CRUDDao;
 import muni.model.Model;
 import muni.service.SubsystemService;
-import muni.util.MockUtil;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,53 +17,125 @@ class IntegServiceImpl implements SubsystemService {
         this.caseDao = caseDao;
     }
 
-    //Person ------------------------------------
-    @Override //TODO Priority-1
-    public Model.Person save(Model.Person in) {
-        long id = personDao.save(in);
-        return personDao.getById(id).get();//Once saved, assumed "guaranteed" return
-    }
-
     @Override
-    public Optional<Model.Person> getPerson(String id) { // Translate integ: string-id to hansen int-id
-        System.out.println("At integServiceImpl person id=" +id);
-        Optional<Model.Person> out = personDao.getById(Long.valueOf(id));
-        System.out.println("At integServiceImpl pers=" +out);
-        return out;
-    }
-
-    @Override
-    public List<Model.Person> findPerson(Model.Person person) {
-        List<Model.Person> out = new ArrayList();
-        out.add(MockUtil.buildPerson());
-        return out;
-    }
-
-    @Override
-    public Optional<Model.PostalAddress> getPostalAddress(String id) {
-        return Optional.ofNullable(MockUtil.buildAddress());
-    }
-
-    @Override
-    public Model.PostalAddress createPostalAddress(String s) {
-        return MockUtil.buildAddress();
-    }
-    //Case ------------------------------------
-
-
-    @Override //TODO Priority-2
-    public Model.Case save(Model.Case in) {
-        throw new UnsupportedOperationException("Please get it implemented");
+    public SubsystemDataAccess<Model.Person> person() {
+        return buildPersonService();
     }
 
 
     @Override
-    public Optional<Model.Case> getCase(String s) {
-        return null;
+    public SubsystemDataAccess<Model.Case> ccase() {
+        return buildCaseService();
     }
 
-    @Override
-    public List<Model.Case> findCase(Model.Case aCase) {
-        return null;
+    private SubsystemDataAccess<Model.Person> buildPersonService() {
+        return new SubsystemDataAccess<Model.Person>() {
+            @Override
+            public Model.Person save(Model.Person in) {
+                long id = personDao.save(in);
+                return personDao.getById(id).get();//Once saved, assumed "guaranteed" return
+            }
+
+            @Override
+            public Optional<Model.Person> get(String id) {
+                System.out.println("At integServiceImpl person id=" + id);
+                Optional<Model.Person> out = personDao.getById(Long.valueOf(id));
+                System.out.println("At integServiceImpl pers=" + out);
+                return out;
+            }
+
+            @Override
+            public List<Model.Person> find(Model.Person person) {
+                List<Model.Person> out = personDao.getAll();//TODO change later
+                return out;
+            }
+
+            @Override
+            public List<Model.Person> recent() {
+                List<Model.Person> out = personDao.getAll();
+                return out;
+            }
+        };
     }
+
+    private SubsystemDataAccess<Model.Case> buildCaseService() {
+        return new SubsystemDataAccess<Model.Case>() {
+            @Override
+            public Model.Case save(Model.Case aCase) {
+                throw new UnsupportedOperationException("Please get it implemented");
+            }
+
+            @Override
+            public Optional<Model.Case> get(String s) {
+                return Optional.empty();
+            }
+
+            @Override
+            public List<Model.Case> find(Model.Case aCase) {
+                return null;
+            }
+
+            @Override
+            public List<Model.Case> recent() {
+                return null;
+            }
+        };
+    }
+
+
+//
+//    //Person ------------------------------------
+//    @Override //TODO Priority-1
+//    public Model.Person save(Model.Person in) {
+//        long id = personDao.save(in);
+//        return personDao.getById(id).get();//Once saved, assumed "guaranteed" return
+//    }
+//
+//    @Override
+//    public Optional<Model.Person> getPerson(String id) { // Translate integ: string-id to hansen int-id
+//        System.out.println("At integServiceImpl person id=" +id);
+//        Optional<Model.Person> out = personDao.getById(Long.valueOf(id));
+//        System.out.println("At integServiceImpl pers=" +out);
+//        return out;
+//    }
+//    @Override
+//    public List<Model.Person> getPersonsRecent() { // Translate integ: string-id to hansen int-id
+//        List<Model.Person> out = personDao.getAll();
+//        return out;
+//    }
+//
+//    @Override
+//    public List<Model.Person> findPerson(Model.Person person) {
+//        List<Model.Person> out = new ArrayList();
+//        out.add(MockUtil.buildPerson());
+//        return out;
+//    }
+//
+//    @Override
+//    public Optional<Model.PostalAddress> getPostalAddress(String id) {
+//        return Optional.ofNullable(MockUtil.buildAddress());
+//    }
+//
+//    @Override
+//    public Model.PostalAddress createPostalAddress(String s) {
+//        return MockUtil.buildAddress();
+//    }
+//    //Case ------------------------------------
+//
+//
+//    @Override //TODO Priority-2
+//    public Model.Case save(Model.Case in) {
+//        throw new UnsupportedOperationException("Please get it implemented");
+//    }
+//
+//
+//    @Override
+//    public Optional<Model.Case> getCase(String s) {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Model.Case> findCase(Model.Case aCase) {
+//        return null;
+//    }
 }
--- src/test/java/integ/test/service/TestIntegService.java
+++ src/test/java/integ/test/service/TestIntegService.java
@@ -21,7 +21,7 @@ public class TestIntegService {
 
     public Model.Person createCustomer(){ // Tests create and get
         Model.Person c1 = Model.Person.newBuilder().setFirstName("Bob").setLastName("Fork").build();
-        final var c1_created = service.save(c1);
+        final var c1_created = service.person().save(c1);
         return c1_created;
     }
 
@@ -31,13 +31,13 @@ public class TestIntegService {
         //valid?
         assertThat(DataQuality.Person.isValidForInsert(c1_new)).isTrue();
         //save
-        Model.Person c1_fromdb = service.save(c1_new);
+        Model.Person c1_fromdb = service.person().save(c1_new);
 
         //update
         final var c1_ToUpdate = Model.Person.newBuilder(c1_fromdb).setLastName("Fork-Updated").setDirty(true).build();
         //valid for update
         assertThat(DataQuality.Person.isValidForUpdate(c1_ToUpdate)).isTrue();
-        final var c1_Updated = service.save(c1_ToUpdate);
+        final var c1_Updated = service.person().save(c1_ToUpdate);
         //System.out.println("updated: " + c1_Updated); //TODO document soprint is triggering xxCase() NoSuchMethod.
         //assertThat(c1_Updated).isSameAs(c1_ToUpdate); //TODO document isSameAs is triggering Caused by: java.lang.NoSuchMethodException: muni.model.Model$Person.getCreateTimeCase()
         assertThat(c1_Updated).extracting(Model.Person::getLastName)
@@ -48,7 +48,7 @@ public class TestIntegService {
 //    @Test
     public void createCase(){
         Model.Case mycase = MockUtil.buildCase();
-        final var createdCase = service.save(mycase);
+        final var createdCase = service.ccase().save(mycase);
         //System.out.println(createdCase);
     }
 
